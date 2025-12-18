11fb849b516f286cab8ae02e0a38cb2b09e87b34
unknown <COMPLAB4@COMPLAB4-23.ASIANCOLLEGE.LOCAL>
2025-12-18 10:00:13 +0800

    third commit

--- app/index.tsx
+++ app/index.tsx
@@ -5,14 +5,8 @@ export default function Index() {
   const name: string = "Perry";
 
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <ScrollView>
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
         <Text style={styles.Text1}>Welcome, {name}</Text>
         <Text style={styles.Text2}>Hello World! Expo</Text>
       </ScrollView>
@@ -21,13 +15,23 @@ export default function Index() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   Text1: {
     color: "red",
     fontSize: 40,
+    marginBottom: 20,
   },
   Text2: {
     backgroundColor: "red",
     color: "white",
     fontSize: 50,
+    padding: 10,
   },
 });
