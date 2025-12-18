6f2b778292ef003512bcb2f5edaa332b1147b4ea
unknown <COMPLAB4@COMPLAB4-23.ASIANCOLLEGE.LOCAL>
2025-12-18 09:46:59 +0800

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,33 @@
+import { Text, StyleSheet, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  const name: string = "Perry";
+
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <ScrollView>
+        <Text style={styles.Text1}>Welcome, {name}</Text>
+        <Text style={styles.Text2}>Hello World! Expo</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  Text1: {
+    color: "red",
+    fontSize: 40,
+  },
+  Text2: {
+    backgroundColor: "red",
+    color: "white",
+    fontSize: 50,
+  },
+});
